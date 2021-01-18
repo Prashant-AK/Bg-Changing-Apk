9ce9d681b1fa60f915b8deeca035b34c26985b00
Prashant-Ak <ps.prashantsingh.98@gmail.com>
2021-01-18 16:48:28 -0500

    Bg Changing apk

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const redArr=['#ffebee','#ffcdd2','#ef9a9a','#e57373','#ef5350','#f44336','#e53935','#d32f2f','#c62828 ','#b71c1c','#ff8a80','#ff5252','#ff1744 ','#d50000']
+
+const App = () => {
+
+	const [randomcolor,setrandomcolor] = useState('rgb(255,255,255)')
+
+	const changeBackG = () => {
+		// console.log("Hello",Math.random()*255);
+		let clr = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
+		setrandomcolor(clr)
+	};
+
+	const redbg=()=>{
+		// console.log();
+		setrandomcolor(redArr[Math.floor(Math.random()*redArr.length)]);
+	}
+	const reset=()=>{
+		setrandomcolor('rgb(255,255,255)');
+	}
+	
+	return (
+		<View style={[styles.container, { backgroundColor: randomcolor }]}>
+			
+			<View style={styles.heading}>
+			<Text style={{ textAlign: 'center', fontSize: 40, paddingTop: 40 }}>Hello It's my first App.</Text>
+			</View>
+			
+
+			<View style={styles.btncontainer}>
+				<TouchableOpacity onPress={()=>changeBackG()} style={[styles.btn,{backgroundColor:'#00FF66'}]}>
+					<Text> Click Me</Text>
+				</TouchableOpacity>
+				
+				<TouchableOpacity onPress={()=>redbg()} style={styles.btn}>
+					<Text> Click for RED</Text>
+				</TouchableOpacity>
+
+				<TouchableOpacity onPress={()=>reset()} style={[styles.btn,{backgroundColor:'orange'}]}>
+					<Text> Reset </Text>
+				</TouchableOpacity>
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	heading:{
+		width:'80%',
+		alignContent:'center',
+		justifyContent:'center'
+		
+	},
+	btncontainer:{
+		flexDirection:'row',
+		width:'80%',
+		alignContent:'center',
+		justifyContent:'space-evenly'
+	},
+	btn:{
+		fontSize: 30,
+		 backgroundColor: 'red',
+		padding:10,  
+	}
 });
+
+export default App;
